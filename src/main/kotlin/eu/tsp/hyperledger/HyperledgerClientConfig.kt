6c1ec81e1cee1090071475920530e501200af653
michal <michal.kit@gmail.com>
2025-08-20 14:14:56 +0100

    Reenabling the HFPublisher connection to the blockchain

--- src/main/kotlin/eu/tsp/hyperledger/HyperledgerClientConfig.kt
+++ src/main/kotlin/eu/tsp/hyperledger/HyperledgerClientConfig.kt
@@ -3,9 +3,10 @@ package eu.tsp.hyperledger
 import eu.tsp.mqtt.requiredProperty
 import io.ktor.server.config.*
 
-data class HyperledgerClientConfig(val caAddress: String, val walletUsername: String)
+data class HyperledgerClientConfig(val caAddress: String, val walletUsername: String, val channelName: String)
 
 fun ApplicationConfig.toHyperledgerClientConfig() = HyperledgerClientConfig(
     caAddress = requiredProperty("caAddress"),
-    walletUsername = requiredProperty("walletUsername")
+    walletUsername = requiredProperty("walletUsername"),
+    channelName = requiredProperty("channelName")
 )
